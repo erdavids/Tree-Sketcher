ce8b51b0c6cad7564a2454e0d4ecceb054bd4fe8
Eric Davidson <erdavids@ncsu.edu>
2017-02-14 12:29:59 -0500

    Parent/Grandparent Functions

--- TreeSketcher.py
+++ TreeSketcher.py
@@ -1,40 +1,52 @@
 from PIL import Image, ImageDraw
 
-
-
 # Used to map the generation of the tree
 class Node(object):
 	def __init__(self, sketcher, position):
 		self.sketcher = sketcher
 		self.x, self.y = position
-		self.parent = []
+		self.parents = []
 		self.grandparents = []
 
-
 		# Will be used to render the tree using the 'nodeList' collection
+
+	def set_parents(self, parents):
+		self.parents = parents
+
+	def add_parent(self, p):
+		self.parents.append(p)
+
+	def set_grandparents(self, grandparents):
+		self.grandparents = grandparents
+
+	def add_grandparent(self, g):
+		self.grandparents.append(g)
+
 class Sketcher(object):
 	def __init__(self, image):
 		self.image = image
 		self.width, self.height = self.image.size
 		self.nodeList = []
 
-		self.root = Node(self, (960, 540), None)
+		self.root = Node(self, (960, 540))
 		self.nodeList.append(self.root)
 
-		self.testNode1 = Node(self, (960, 600), self.root)
+		self.testNode1 = Node(self, (960, 600))
 		self.nodeList.append(self.testNode1)
+		self.testNode1.add_parent(self.root)
 
 		draw = ImageDraw.Draw(self.image)
 		for node in self.nodeList:
-			if (node.parent != None):
-				draw.line((node.x, node.y, node.parent.x, node.parent.y), fill=128)
-
-
-
+			if node.parents:
+				for p in node.parents:
+					draw.line((node.x, node.y, p.x, p.y), fill=128)
+			if node.grandparents:
+				for g in node.grandparents:
+					draw.line((node.x, node.y, g.x, g.y), fill=128)
 
 
 def main():
-	new_image = Image.new('RGB', (1920, 1080), (0, 0, 0))
+	new_image = Image.new('RGB', (1920, 1080), (255, 255, 255))
 
 	sketcher = Sketcher(new_image);
 
